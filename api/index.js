7d0f6b38e912e03ca670620e980c272fe7e2e011
JCPrime01 <vanzokiller@gmail.com>
2026-06-03 13:49:20 -0300

    chore: shareCode 56SLIT19RHA -> NE0CZ8WB3SZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=56SLIT19RHA&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=NE0CZ8WB3SZ&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
